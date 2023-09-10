75c5724dc81e7421b5d4d650ec9968d36b56c0d2
Michael Kolkov <55915846+mireabot@users.noreply.github.com>
2023-09-09 20:03:26 -0400

    Changed project to SwiftUI

---
+++ Expensaro/ExpensaroApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensaroApp.swift
+//  Expensaro
+//
+//  Created by Mikhail Kolkov on 9/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpensaroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
