825327117e40b633c2a8691155bacf7742fcd53e
Michael Kolkov <55915846+mireabot@users.noreply.github.com>
2024-01-18 20:57:18 -0500

    Created enum with success rate results, Designed card UI

--- Expensaro/Managers/GoalMathManager.swift
+++ Expensaro/Managers/GoalMathManager.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Combine
+import SwiftUI
 
 final class GoalMathManager: ObservableObject {
   static let shared = GoalMathManager()
@@ -23,6 +24,45 @@ final class GoalMathManager: ObservableObject {
     
     DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) {
       self.isLoading = false
+    }
+  }
+}
+
+private enum GoalSuccessRate {
+  case noRisk
+  case moderateRisk
+  case highRisk
+  
+  var title: String {
+    switch self {
+    case .noRisk:
+      return "No risk"
+    case .moderateRisk:
+      return "Moderate risk"
+    case .highRisk:
+      return "High risk"
+    }
+  }
+  
+  var text: String {
+    switch self {
+    case .noRisk:
+      return "Your financials are looking healthy for this goal. It's a favorable time to get started!"
+    case .moderateRisk:
+      return "With careful budgeting and planning, initiating this goal could be feasible"
+    case .highRisk:
+      return "Starting this goal now will be challenging. It may be wise to wait until your situation improves"
+    }
+  }
+  
+  var color: Color {
+    switch self {
+    case .noRisk:
+      return .green
+    case .moderateRisk:
+      return .yellow
+    case .highRisk:
+      return .red
     }
   }
 }
--- Expensaro/Modules/Goals Module/Helpers/GoalEstimatorView.swift
+++ Expensaro/Modules/Goals Module/Helpers/GoalEstimatorView.swift
@@ -11,28 +11,88 @@ import ExpensaroUIKit
 struct GoalEstimatorView: View {
   @StateObject var goalManager = GoalMathManager.shared
   var body: some View {
-    EXBaseCard {
-      VStack(alignment: .leading, spacing: 10) {
-        VStack(alignment: .leading, spacing: 3) {
-          Text("57%")
-            .font(.title3Bold)
-          Text("You have good chances to make this goal")
-            .font(.calloutRegular)
-            .foregroundColor(.darkGrey)
+    VStack {
+      EXBaseCard {
+        VStack(alignment: .leading, spacing: 10) {
+          HStack(alignment: .top, spacing: 0, content: {
+            VStack(alignment: .leading, spacing: 3) {
+              Text("Goal success rate")
+                .font(.footnoteRegular)
+                .foregroundColor(.darkGrey)
+              Text("24%")
+                .font(.title2Bold)
+            }
+            Spacer()
+            Button(action: {}, label: {
+              Image(systemName: "arrow.clockwise")
+                .foregroundColor(.primaryGreen)
+            })
+          })
+          Divider()
+          VStack(alignment: .leading, spacing: 3, content: {
+            Text("High risk")
+              .font(.headlineBold)
+              .foregroundColor(.red)
+            Text("Starting this goal now will be challenging. It may be wise to wait until your situation improves")
+              .font(.footnoteRegular)
+              .foregroundColor(.darkGrey)
+          })
         }
-        HStack {
-          Rectangle()
-            .fill(.green)
-            .frame(height: 20)
-            .cornerRadius(5)
-          Rectangle()
-            .fill(.yellow)
-            .frame(height: 20)
-            .cornerRadius(5)
-          Rectangle()
-            .fill(.red)
-            .frame(height: 20)
-            .cornerRadius(5)
+      }
+      
+      EXBaseCard {
+        VStack(alignment: .leading, spacing: 10) {
+          HStack(alignment: .top, spacing: 0, content: {
+            VStack(alignment: .leading, spacing: 3) {
+              Text("Goal success rate")
+                .font(.footnoteRegular)
+                .foregroundColor(.darkGrey)
+              Text("57%")
+                .font(.title2Bold)
+            }
+            Spacer()
+            Button(action: {}, label: {
+              Image(systemName: "arrow.clockwise")
+                .foregroundColor(.primaryGreen)
+            })
+          })
+          Divider()
+          VStack(alignment: .leading, spacing: 3, content: {
+            Text("Moderate risk")
+              .font(.headlineBold)
+              .foregroundColor(.yellow)
+            Text("With careful budgeting and planning, initiating this goal could be feasible")
+              .font(.footnoteRegular)
+              .foregroundColor(.darkGrey)
+          })
+        }
+      }
+      
+      EXBaseCard {
+        VStack(alignment: .leading, spacing: 10) {
+          HStack(alignment: .top, spacing: 0, content: {
+            VStack(alignment: .leading, spacing: 3) {
+              Text("Goal success rate")
+                .font(.footnoteRegular)
+                .foregroundColor(.darkGrey)
+              Text("78%")
+                .font(.title2Bold)
+            }
+            Spacer()
+            Button(action: {}, label: {
+              Image(systemName: "arrow.clockwise")
+                .foregroundColor(.primaryGreen)
+            })
+          })
+          Divider()
+          VStack(alignment: .leading, spacing: 3, content: {
+            Text("No risk")
+              .font(.headlineBold)
+              .foregroundColor(.green)
+            Text("Your financials are looking healthy for this goal. It's a favorable time to get started!")
+              .font(.footnoteRegular)
+              .foregroundColor(.darkGrey)
+          })
         }
       }
     }
